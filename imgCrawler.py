9e1b6c8b6a44f5a55dbfa34718e2720edd522d8b
Leon <Leongrasmeijer@gmail.com>
2014-12-15 18:48:06 +0100

    Added option to download images

--- imgCrawler.py
+++ imgCrawler.py
@@ -1,12 +1,13 @@
 import praw
-import subprocess, os
+import urllib
 r = praw.Reddit(user_agent='Image crawler by /u/leonlg v1.0.2')
 
 """ Choose number of posts and what subreddit here """
-number_of_images = raw_input("How many images do you want?")
-subreddit = raw_input("What subreddit do you want to crawl?")
+number_of_images = raw_input("How many images do you want? ")
+subreddit = raw_input("What subreddit do you want to crawl? ")
+download = raw_input("Do you want to download these images? (y/n) ")
 
-submissions = r.get_subreddit(subreddit).get_hot(limit=number_of_images)
+submissions = r.get_subreddit(subreddit).get_hot(limit=int(number_of_images))
 
 """ Open files where to put images """
 stylesheet = open('style.css', 'w')
@@ -19,6 +20,11 @@ for i, sub in enumerate(submissions):
     if not sub.is_self and sub.url.endswith(".jpg") or sub.url.endswith(".png"):
         style += ' #img%d { background: url(%s); }' % ( i, sub.url )
         content += '<div id="img%d" class="img"></div>\n' % i
+        if download == "y":
+            f = urllib.urlopen(sub.url)
+            with open("images/img%d" % i, "wb") as imgFile:
+                imgFile.write(f.read())
+            f.close()
 
 for k in range(int(number_of_images)):
     style += '#img%d,' % k
